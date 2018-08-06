5cec6d1667cda76bfcdd83e25b9932376e27d72a
eyulcao <841598151@qq.com>
2018-08-07 00:22:39 +0800

    commit 5

--- pythonnew.py
+++ pythonnew.py
@@ -7,5 +7,5 @@ while True:
 # 判断start的值若其为51，则说明上一次已经输出了100，跳出循环
    if start==51:
      break
-   print start*2
+   print(start*2)
    start +=1
--- pythontest.py
+++ pythontest.py
@@ -1,4 +1,3 @@
 import pymongo
 print('welcome to cylpython')
 print('cyl python')
-print('github commit')
